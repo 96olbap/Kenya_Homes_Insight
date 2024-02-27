75be0a35846f0f19c74eecb8cd1d510408a91b2d
96olbap <alvinwanjala@gmail.com>
2024-02-28 02:12:45 +0300

    Created the scraper function to scrape the website data and create a dataframe to display the data

--- scraper.py
+++ scraper.py
@@ -35,11 +35,32 @@ def ScrapePropertyData(): #Function to scrape property data
     driver.get('https://www.buyrentkenya.com/houses-for-sale/nairobi') #website to fetch data from
     try: #exception to wait for 30 seconds for the content on the target page to be loaded
         elem = WebDriverWait(driver, 30).until(
-    EC.presence_of_element_located((By.ID, "mainContent")) #the id encompassing the content on the target page
+    EC.presence_of_element_located((By.ID, "mainContent")) #the class encompassing the content on the target page
     )
     finally:
         print('loaded')
         soup = BeautifulSoup(driver.page_source, 'html.parser')
 
     """Scraper to fetch each row and column"""
-    
+
+    result = soup.find_all('div', {'class' : 'listing-card'})
+    len(result)
+
+    result_update = [i for i in result if i.has_attr('data-bi')]
+    len(result_update)
+
+    """To get the specific attributes"""
+    category = [result.find('div', {'data-bi-listing-category'}).get_text() for result in result_update]
+    price = [result.find('div', {'data-bi-listing-price'}).get_text() for result in result_update]
+    title = [result.find('a', {'data-cy' : 'listing-title-link'}).get_text() for result in result_update]
+    location = [result.find('p', {'class' : 'ml-1'}).get_text() for result in result_update]
+    bedrooms = [result.find('span', {'data-cy' : 'card-beds'}).get_text() for result in result_update]
+    bathrooms = [result.find('span', {'data-cy' : 'card-bathrooms'}).get_text() for result in result_update]
+
+    """To create a dataframe"""
+    real_estate = pd.DataFrame(columns = ['Title', 'Category', 'Location', 'Beds', 'Baths', 'Price'])
+
+    for i in range (len(location)):
+        real_estate = real_estate.append({'Title':title[i], 'Category':category[i], 'Location':location[i], 'Beds':bedrooms[i], 'Baths':bathrooms[i], 'Price':price[i]})
+    real_estate
+
