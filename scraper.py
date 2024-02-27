f06c28256c5bf1dc76e0a9005a3e25641cbbc0bb
96olbap <alvinwanjala@gmail.com>
2024-02-27 14:44:33 +0300

    Install and set up dependencies

---
+++ scraper.py
@@ -0,0 +1,16 @@
+import requests
+import json
+import pandas as pd
+import bs4 as BeautifulSoup
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+import chromedriver_autoinstaller
+
+from pyvirtualdisplay import Display
