a38af0a9079754b2382235b6d0f51ec44a65d3a4
96olbap <alvinwanjala@gmail.com>
2024-05-04 14:36:26 +0300

    set up a url for the search view

--- appdir/realestateproject/realestateproject/urls.py
+++ appdir/realestateproject/realestateproject/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from listings.views import search
+
+import listings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('search/', search, name='search'),
 ]
