8554c6bc22223e009ee4e0fde862712899118240
96olbap <alvinwanjala@gmail.com>
2024-05-04 14:30:11 +0300

    create a view for searching through property listings

--- appdir/realestateproject/listings/views.py
+++ appdir/realestateproject/listings/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import House
 
-# Create your views here.
+def search(request):
+    beds = request.GET.get('beds', 0)
+    baths = request.GET.get('baths', 0)
+    location = request.GET.get('location', '')
+
+    houses = House.objects.filter(beds__gte=beds, baths__gte=baths, location__icontains=location)
+    data = list(houses.values())
+    return JsonResponse(data, safe=False)
