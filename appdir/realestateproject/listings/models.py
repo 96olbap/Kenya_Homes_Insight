c8ca2465c6c8b69a3e4326e6cfb72c08c3d09fac
96olbap <alvinwanjala@gmail.com>
2024-05-04 14:21:29 +0300

    make migrations

--- appdir/realestateproject/listings/models.py
+++ appdir/realestateproject/listings/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class House(models.Model):
+    beds = models.FloatField()
+    baths = models.FloatField()
+    price = models.FloatField()
+    location = models.CharField(max_length=100)
+    bedroom_ratio = models.FloatField()
+
+    def __str__(self):
+        return f"{self.location} House with {self.beds} Beds and {self.baths} Baths"
