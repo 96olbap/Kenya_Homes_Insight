05f28653b0e9ae36a448e75d4ec472dc6562eda1
96olbap <alvinwanjala@gmail.com>
2024-05-07 00:12:39 +0300

    This commit creates the management command for the listings app and updates the model structure

--- appdir/realestateproject/listings/models.py
+++ appdir/realestateproject/listings/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
-class House(models.Model):
+class Listing(models.Model):
+    title = models.CharField(max_length=150)
+    category = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
     beds = models.FloatField()
     baths = models.FloatField()
     price = models.FloatField()
-    location = models.CharField(max_length=100)
-    bedroom_ratio = models.FloatField()
-
+    
     def __str__(self):
         return f"{self.location} House with {self.beds} Beds and {self.baths} Baths"
