de213493ab501dd2d9c909ac9d42f37dd33b11d1
96olbap <alvinwanjala@gmail.com>
2024-05-24 13:46:29 +0300

    fixed homepage herosection slideshow

--- nyumba_project/static/js/custom.js
+++ nyumba_project/static/js/custom.js
@@ -5,9 +5,9 @@
 
     // HERO SLIDE
     $('.hero-slide').backstretch([
-      "images/slideshow/white-wall-living-room-have-sofa-decoration-3d-rendering.jpg", 
-      "images/slideshow/interior-wall-mockup-with-sofa-cabinet-living-room-with-empty-white-wall-background-3d-rendering.jpg",
-      "images/slideshow/wood-sideboard-living-room-interior-with-copy-space.jpg"
+      "static/images/slideshow/white-wall-living-room-have-sofa-decoration-3d-rendering.jpg", 
+      "static/images/slideshow/interior-wall-mockup-with-sofa-cabinet-living-room-with-empty-white-wall-background-3d-rendering.jpg",
+      "static/images/slideshow/wood-sideboard-living-room-interior-with-copy-space.jpg"
     ],  {duration: 2000, fade: 750});
 
     // REVIEWS CAROUSEL
